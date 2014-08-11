b3392dcd8173ba826569efa5b08a45c9e791f16c
Johan Vervloet <johan.vervloet@gmail.com>
2014-08-11 22:09:27 +0200

    At least this shows something.
    I am still clueless tough :-)

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,12 @@ var AppRouter = Backbone.Router.extend({
 
   initialize: function() {
     this.players = new PlayerCollection();
-    this.addResultView = new AddResultView({collection: this.players, el: "#result"});
+    this.addResultView = new AddResultView({collection: this.players});
 
     // Add some default players for now.
+    // (The thing below does not work)
+
+    /*
     for (i = 1; i <= 4; ++i) {
       var player = new Player();
       player.set({
@@ -13,6 +16,7 @@ var AppRouter = Backbone.Router.extend({
       });
       this.players.add(player);
     }
+    */
   },
 
   // no routes for the moment
@@ -21,13 +25,15 @@ var AppRouter = Backbone.Router.extend({
   },
 
   index: function() {
-    this.addResultView.render();
+    //this.addResultView.render();
+    $('#result').html(this.addResultView.render().el);
   }
 });
 
 Player = Backbone.Model.extend({
   defaults: {
-    code: 'XXX'
+    code: 'XXX',
+    wins: 0, // -1: loses, 0 nothing, 1 wins
   }
 });
 
@@ -48,19 +54,25 @@ PlayerCheckboxView = Backbone.View.extend({
   }
 });
 
-AddResultsView = Backbone.View.extend({
+AddResultView = Backbone.View.extend({
+  tagName: "div",
+  className: "div",
   template: _.template($('#resultInput').html()),
   initialize: function() {
     // no clue about this:
-    this.model.bind("reset", this.render, this);
+    //this.model.bind("reset", this.render, this);
+    _.bindAll(this, "render");
   },
 
   render: function() {
     $(this.el).html(this.template());
-    this.collection.each(function(model){
+    _.each(this.collection.players, function(model){
       var view = new PlayerCheckboxView({model: model});
       $(this.el).find("#winners").append(view);
     });
     return this;
   }
 });
+
+appRouter = new AppRouter();
+Backbone.history.start();
